b584bd850cbfb8183ca4582e3cbebadb56dd4336
malte.martens <malte.martens@iu-study.org>
2026-01-27 11:14:22 +0100

    Add Squirrel Bank

---
+++ src/student_projects/g02/data/engine/mathKernel.py
@@ -0,0 +1,7 @@
+from abc import ABC, abstractmethod
+
+class MathKernel(ABC):
+
+    @abstractmethod
+    def find_stolen_nuts(self, expected, actual):
+        pass
---
+++ src/student_projects/g02/data/storage/dataGenerator.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+class DataGenerator:
+    @staticmethod
+    def generate_dataset(size: int) -> dict:
+        rng = np.random.default_rng(42)
+
+        return {
+            "id": np.arange(size),
+            "coords_x": rng.random(size) * 100,
+            "coords_y": rng.random(size) * 100,
+            "nut_type": rng.integers(0, 5, size),
+            "depth_cm": rng.random(size) * 50,
+            "amount": rng.integers(1, 100, size),
+            "expected_amount": rng.integers(50, 120, size)
+        }
